b9f280c52dc06404530ce1e4954e9feae4d27d8d
Samuel Dandy <sam.dandy@tigers.com>
2025-08-10 20:34:27 -0500

    fmt

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
 )
 
 func Handler(r *chi.Mux) {
+	fmt.Println("Initializing handlers...")
 	r.Use(chimiddle.StripSlashes)
 	r.Route("/avg_price", func(router chi.Router) {
 		router.Get("/", GetAvgPrice)
